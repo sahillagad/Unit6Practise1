1363219392629d73e73510d9a37475becc0b2fcf
sahillagad <sahillagad9@gmail.com>
2023-01-11 18:20:34 +0530

    w
    w

---
+++ 10_TablePerSubClass/src/main/java/com/masai/Utility/EMUtility.java
@@ -0,0 +1,29 @@
+package com.masai.Utility;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EMUtility {
+
+	
+	
+	private static EntityManagerFactory emf;
+	
+	
+	static {
+		
+		emf=Persistence.createEntityManagerFactory("Unit");
+		
+	}
+	
+	public static  EntityManager provideEmEntityManager() {
+		
+		
+		EntityManager em=emf.createEntityManager();
+		
+		
+		
+		return em;
+	}
+}
